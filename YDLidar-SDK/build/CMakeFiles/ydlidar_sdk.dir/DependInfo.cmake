7881ba21bc1c38d5f031d61af97ae76d4a3dd53e
Harshvardhan Gupta <harshvardhansgupta@gmail.com>
2025-01-14 23:16:59 +0530

    files

---
+++ YDLidar-SDK/build/CMakeFiles/ydlidar_sdk.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/rover_1/YDLidar-SDK/core/serial/impl/unix/lock.c" "/home/rover_1/YDLidar-SDK/build/CMakeFiles/ydlidar_sdk.dir/core/serial/impl/unix/lock.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../core"
+  "../src"
+  "../"
+  "."
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rover_1/YDLidar-SDK/core/base/timer.cpp" "/home/rover_1/YDLidar-SDK/build/CMakeFiles/ydlidar_sdk.dir/core/base/timer.cpp.o"
+  "/home/rover_1/YDLidar-SDK/core/common/ydlidar_def.cpp" "/home/rover_1/YDLidar-SDK/build/CMakeFiles/ydlidar_sdk.dir/core/common/ydlidar_def.cpp.o"
+  "/home/rover_1/YDLidar-SDK/core/network/ActiveSocket.cpp" "/home/rover_1/YDLidar-SDK/build/CMakeFiles/ydlidar_sdk.dir/core/network/ActiveSocket.cpp.o"
+  "/home/rover_1/YDLidar-SDK/core/network/PassiveSocket.cpp" "/home/rover_1/YDLidar-SDK/build/CMakeFiles/ydlidar_sdk.dir/core/network/PassiveSocket.cpp.o"
+  "/home/rover_1/YDLidar-SDK/core/network/SimpleSocket.cpp" "/home/rover_1/YDLidar-SDK/build/CMakeFiles/ydlidar_sdk.dir/core/network/SimpleSocket.cpp.o"
+  "/home/rover_1/YDLidar-SDK/core/serial/impl/unix/list_ports_linux.cpp" "/home/rover_1/YDLidar-SDK/build/CMakeFiles/ydlidar_sdk.dir/core/serial/impl/unix/list_ports_linux.cpp.o"
+  "/home/rover_1/YDLidar-SDK/core/serial/impl/unix/unix_serial.cpp" "/home/rover_1/YDLidar-SDK/build/CMakeFiles/ydlidar_sdk.dir/core/serial/impl/unix/unix_serial.cpp.o"
+  "/home/rover_1/YDLidar-SDK/core/serial/serial.cpp" "/home/rover_1/YDLidar-SDK/build/CMakeFiles/ydlidar_sdk.dir/core/serial/serial.cpp.o"
+  "/home/rover_1/YDLidar-SDK/src/CYdLidar.cpp" "/home/rover_1/YDLidar-SDK/build/CMakeFiles/ydlidar_sdk.dir/src/CYdLidar.cpp.o"
+  "/home/rover_1/YDLidar-SDK/src/ETLidarDriver.cpp" "/home/rover_1/YDLidar-SDK/build/CMakeFiles/ydlidar_sdk.dir/src/ETLidarDriver.cpp.o"
+  "/home/rover_1/YDLidar-SDK/src/GSLidarDriver.cpp" "/home/rover_1/YDLidar-SDK/build/CMakeFiles/ydlidar_sdk.dir/src/GSLidarDriver.cpp.o"
+  "/home/rover_1/YDLidar-SDK/src/SDMLidarDriver.cpp" "/home/rover_1/YDLidar-SDK/build/CMakeFiles/ydlidar_sdk.dir/src/SDMLidarDriver.cpp.o"
+  "/home/rover_1/YDLidar-SDK/src/YDlidarDriver.cpp" "/home/rover_1/YDLidar-SDK/build/CMakeFiles/ydlidar_sdk.dir/src/YDlidarDriver.cpp.o"
+  "/home/rover_1/YDLidar-SDK/src/filters/NoiseFilter.cpp" "/home/rover_1/YDLidar-SDK/build/CMakeFiles/ydlidar_sdk.dir/src/filters/NoiseFilter.cpp.o"
+  "/home/rover_1/YDLidar-SDK/src/filters/StrongLightFilter.cpp" "/home/rover_1/YDLidar-SDK/build/CMakeFiles/ydlidar_sdk.dir/src/filters/StrongLightFilter.cpp.o"
+  "/home/rover_1/YDLidar-SDK/src/ydlidar_sdk.cpp" "/home/rover_1/YDLidar-SDK/build/CMakeFiles/ydlidar_sdk.dir/src/ydlidar_sdk.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../core"
+  "../src"
+  "../"
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
